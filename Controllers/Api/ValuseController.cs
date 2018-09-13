f18202cf8a7c053475828d357bdabce01b5f6e2b
wugelis <wergelis@mail.tbcnet.net>
2018-09-13 14:02:06 +0800

    撰寫完成 DI for IHttpClientFactory

---
+++ Controllers/Api/ValuseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebCore21TestApp2.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController: ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetStringAsync("http://www.google.com");
+            return Ok(result);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# ASP.NET Core 2.1 開發指南 中的 DI for IHttpClientFactory" 
